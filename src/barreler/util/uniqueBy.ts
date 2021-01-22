feb3dbb25e1869e39a3223c63d9bd60dc89095b5
minidonut <nycom13@gmail.com>
2021-01-22 21:51:25 +0900

    use const

--- src/barreler/util/uniqueBy.ts
+++ src/barreler/util/uniqueBy.ts
@@ -1,6 +1,6 @@
 export function uniqueBy<T>(hash: (x: T) => string | number, arr: T[]): T[] {
-  let result = [];
-  let table: Record<string, boolean> = {};
+  const result = [];
+  const table: Record<string, boolean> = {};
 
   for (const item of arr) {
     const key = hash(item);
